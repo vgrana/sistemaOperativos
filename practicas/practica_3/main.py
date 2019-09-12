717868107c563869b266c04852e5966e8a0ce58f
a <aa>
2019-09-12 13:30:38 -0300

    practica 3

---
+++ practicas/practica_3/main.py
@@ -0,0 +1,32 @@
+from hardware import *
+from so import *
+import log
+
+
+##
+##  MAIN 
+##
+if __name__ == '__main__':
+    log.setupLogger()
+    log.logger.info('Starting emulator')
+
+    ## setup our hardware and set memory size to 25 "cells"
+    HARDWARE.setup(25)
+
+    ## Switch on computer
+    HARDWARE.switchOn()
+
+    ## new create the Operative System Kernel
+    # "booteamos" el sistema operativo
+    kernel = Kernel()
+
+    ##  create a program
+    prg = Program("test.exe", [ASM.CPU(2), ASM.IO(), ASM.CPU(3), ASM.IO(), ASM.CPU(3)])
+    
+    # execute the program
+    kernel.run(prg)
+
+
+
+
+
---
+++ practicas/practica_3/so.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+from hardware import *
+import log
+
+
+
+## emulates a compiled program
+class Program():
+
+    def __init__(self, name, instructions):
+        self._name = name
+        self._instructions = self.expand(instructions)
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def instructions(self):
+        return self._instructions
+
+    def addInstr(self, instruction):
+        self._instructions.append(instruction)
+
+    def expand(self, instructions):
+        expanded = []
+        for i in instructions:
+            if isinstance(i, list):
+                ## is a list of instructions
+                expanded.extend(i)
+            else:
+                ## a single instr (a String)
+                expanded.append(i)
+
+        ## now test if last instruction is EXIT
+        ## if not... add an EXIT as final instruction
+        last = expanded[-1]
+        if not ASM.isEXIT(last):
+            expanded.append(INSTRUCTION_EXIT)
+
+        return expanded
+
+    def __repr__(self):
+        return "Program({name}, {instructions})".format(name=self._name, instructions=self._instructions)
+
+
+## emulates an Input/Output device controller (driver)
+class IoDeviceController():
+
+    def __init__(self, device):
+        self._device = device
+        self._waiting_queue = []
+        self._currentPCB = None
+
+    def runOperation(self, pcb, instruction):
+        pair = {'pcb': pcb, 'instruction': instruction}
+        # append: adds the element at the end of the queue
+        self._waiting_queue.append(pair)
+        # try to send the instruction to hardware's device (if is idle)
+        self.__load_from_waiting_queue_if_apply()
+
+    def getFinishedPCB(self):
+        finishedPCB = self._currentPCB
+        self._currentPCB = None
+        self.__load_from_waiting_queue_if_apply()
+        return finishedPCB
+
+    def __load_from_waiting_queue_if_apply(self):
+        if (len(self._waiting_queue) > 0) and self._device.is_idle:
+            ## pop(): extracts (deletes and return) the first element in queue
+            pair = self._waiting_queue.pop(0)
+            #print(pair)
+            pcb = pair['pcb']
+            instruction = pair['instruction']
+            self._currentPCB = pcb
+            self._device.execute(instruction)
+
+
+    def __repr__(self):
+        return "IoDeviceController for {deviceID} running: {currentPCB} waiting: {waiting_queue}".format(deviceID=self._device.deviceId, currentPCB=self._currentPCB, waiting_queue=self._waiting_queue)
+
+## emulates the  Interruptions Handlers
+class AbstractInterruptionHandler():
+    def __init__(self, kernel):
+        self._kernel = kernel
+
+    @property
+    def kernel(self):
+        return self._kernel
+
+    def execute(self, irq):
+        log.logger.error("-- EXECUTE MUST BE OVERRIDEN in class {classname}".format(classname=self.__class__.__name__))
+
+
+class KillInterruptionHandler(AbstractInterruptionHandler):
+
+    def execute(self, irq):
+        log.logger.info(" Program Finished ")
+        HARDWARE.cpu.pc = -1  ## dejamos el CPU IDLE
+
+class IoInInterruptionHandler(AbstractInterruptionHandler):
+
+    def execute(self, irq):
+        operation = irq.parameters
+        pcb = {'pc': HARDWARE.cpu.pc} # porque hacemos esto ???
+        HARDWARE.cpu.pc = -1   ## dejamos el CPU IDLE
+        self.kernel.ioDeviceController.runOperation(pcb, operation)
+        log.logger.info(self.kernel.ioDeviceController)
+
+
+class IoOutInterruptionHandler(AbstractInterruptionHandler):
+
+    def execute(self, irq):
+        pcb = self.kernel.ioDeviceController.getFinishedPCB()
+        HARDWARE.cpu.pc = pcb['pc']
+        log.logger.info(self.kernel.ioDeviceController)
+
+
+# emulates the core of an Operative System
+class Kernel():
+
+    def __init__(self):
+        ## setup interruption handlers
+        killHandler = KillInterruptionHandler(self)
+        HARDWARE.interruptVector.register(KILL_INTERRUPTION_TYPE, killHandler)
+
+        ioInHandler = IoInInterruptionHandler(self)
+        HARDWARE.interruptVector.register(IO_IN_INTERRUPTION_TYPE, ioInHandler)
+
+        ioOutHandler = IoOutInterruptionHandler(self)
+        HARDWARE.interruptVector.register(IO_OUT_INTERRUPTION_TYPE, ioOutHandler)
+
+        ## controls the Hardware's I/O Device
+        self._ioDeviceController = IoDeviceController(HARDWARE.ioDevice)
+
+
+    @property
+    def ioDeviceController(self):
+        return self._ioDeviceController
+
+    def load_program(self, program):
+        # loads the program in main memory
+        progSize = len(program.instructions)
+        for index in range(0, progSize):
+            inst = program.instructions[index]
+            HARDWARE.memory.write(index, inst)
+
+    ## emulates a "system call" for programs execution
+    def run(self, program):
+        self.load_program(program)
+        log.logger.info("\n Executing program: {name}".format(name=program.name))
+        log.logger.info(HARDWARE)
+
+        # set CPU program counter at program's first intruction
+        HARDWARE.cpu.pc = 0
+
+
+    def __repr__(self):
+        return "Kernel "
